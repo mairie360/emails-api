b71b25017c86e1e8a4d2b466a374bc51702b510a
Quentin Tennerel <tennerelquentin@gmail.com>
2025-03-20 11:14:32 +0100

    Feat : add tables for mail module database

--- migrations/2025-03-17-120146_create_emails/up.sql
+++ migrations/2025-03-17-120146_create_emails/up.sql
@@ -1,4 +1,19 @@
 -- Your SQL goes here
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(320) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE user_mail_boxes (
+    user_id INT NOT NULL,
+    token JSONB NOT NULL,
+    type enum('gmail', 'outlook', 'orange', 'custom') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    PRIMARY KEY (user_id)
+);
+
 CREATE TABLE emails (
     id SERIAL PRIMARY KEY,
     sender VARCHAR(320) NOT NULL,
@@ -7,4 +22,12 @@ CREATE TABLE emails (
     body TEXT,
     is_read BOOLEAN DEFAULT FALSE,
     received_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE email_mail_box (
+    email_id INT NOT NULL,
+    mail_box_id INT NOT NULL,
+    FOREIGN KEY (email_id) REFERENCES emails(id),
+    FOREIGN KEY (mail_box_id) REFERENCES user_mail_boxes(id),
+    PRIMARY KEY (email_id, mail_box_id)
 );
