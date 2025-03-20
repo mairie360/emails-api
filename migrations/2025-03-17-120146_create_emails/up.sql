84de41489e1352ce38f0111fa9c8088a19550a36
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-03-20 19:21:12 +0400

    Fix: Comment some code

--- migrations/2025-03-17-120146_create_emails/up.sql
+++ migrations/2025-03-17-120146_create_emails/up.sql
@@ -1,18 +1,18 @@
 -- Your SQL goes here
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(320) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE users (
+--     id SERIAL PRIMARY KEY,
+--     email VARCHAR(320) NOT NULL,
+--     password VARCHAR(60) NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
 
-CREATE TABLE user_mail_boxes (
-    user_id INT NOT NULL,
-    token JSONB NOT NULL,
-    type enum('gmail', 'outlook', 'orange', 'custom') NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    PRIMARY KEY (user_id)
-);
+-- CREATE TABLE user_mail_boxes (
+--     user_id INT NOT NULL,
+--     token JSONB NOT NULL,
+--     type enum('gmail', 'outlook', 'orange', 'custom') NOT NULL,
+--     FOREIGN KEY (user_id) REFERENCES users(id),
+--     PRIMARY KEY (user_id)
+-- );
 
 CREATE TABLE emails (
     id SERIAL PRIMARY KEY,
@@ -24,10 +24,10 @@ CREATE TABLE emails (
     received_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE email_mail_box (
-    email_id INT NOT NULL,
-    mail_box_id INT NOT NULL,
-    FOREIGN KEY (email_id) REFERENCES emails(id),
-    FOREIGN KEY (mail_box_id) REFERENCES user_mail_boxes(id),
-    PRIMARY KEY (email_id, mail_box_id)
-);
+-- CREATE TABLE email_mail_box (
+--     email_id INT NOT NULL,
+--     mail_box_id INT NOT NULL,
+--     FOREIGN KEY (email_id) REFERENCES emails(id),
+--     FOREIGN KEY (mail_box_id) REFERENCES user_mail_boxes(id),
+--     PRIMARY KEY (email_id, mail_box_id)
+-- );
